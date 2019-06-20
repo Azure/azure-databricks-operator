069fd0e3132f86a975cdba9dedfd4419f47370d8
Xinyun Zhou <me@xyzhou.com>
2019-06-21 08:21:16 +1000

    organise codebase

--- pkg/controller/notebookjob/notebookjob_controller.go
+++ pkg/controller/notebookjob/notebookjob_controller.go
@@ -20,20 +20,14 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strings"
-	"time"
-
-	v1 "k8s.io/api/core/v1"
 
 	microsoftv1beta1 "microsoft/azure-databricks-operator/pkg/apis/microsoft/v1beta1"
 
 	db "github.com/xinsnake/databricks-sdk-golang"
 	dbazure "github.com/xinsnake/databricks-sdk-golang/azure"
-	dbmodels "github.com/xinsnake/databricks-sdk-golang/azure/models"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -143,7 +137,7 @@ func (r *ReconcileNotebookJob) Reconcile(request reconcile.Request) (reconcile.R
 	}
 
 	if !instance.IsRunning() {
-		err = r.submitRunToAPI(instance)
+		err = r.submitRunToDatabricks(instance)
 		if err != nil {
 			return reconcile.Result{}, fmt.Errorf("error when submitting job to API: %v", err)
 		}
@@ -181,133 +175,5 @@ func (r *ReconcileNotebookJob) handleFinalizer(instance *microsoftv1beta1.Notebo
 
 func (r *ReconcileNotebookJob) deleteExternalDependency(instance *microsoftv1beta1.NotebookJob) error {
 	log.Info("deleting the external dependencies")
-	runID := instance.Spec.NotebookTask.RunID
-	if runID != 0 {
-		err := r.apiClient.Jobs().RunsCancel(int64(runID))
-		if err != nil {
-			return err
-		}
-		time.Sleep(10 * time.Second)
-		return r.apiClient.Jobs().RunsDelete(int64(runID))
-	}
-	return nil
-}
-
-func (r *ReconcileNotebookJob) submitRunToAPI(instance *microsoftv1beta1.NotebookJob) error {
-
-	// runName
-	var runName = instance.ObjectMeta.Name
-
-	// clusterSpec
-	clusterSpec := dbmodels.ClusterSpec{
-		NewCluster: &dbmodels.NewCluster{
-			SparkVersion: "5.2.x-scala2.11",
-			NodeTypeID:   "Standard_DS3_v2",
-			NumWorkers:   3,
-			SparkEnvVars: &dbmodels.SparkEnvPair{
-				Key:   "PYSPARK_PYTHON",
-				Value: "/databricks/python3/bin/python3",
-			},
-		},
-	}
-	if instance.Spec.ClusterSpec.SparkVersion != "" {
-		clusterSpec.NewCluster.SparkVersion = instance.Spec.ClusterSpec.SparkVersion
-	}
-	if instance.Spec.ClusterSpec.NodeTypeId != "" {
-		clusterSpec.NewCluster.NodeTypeID = instance.Spec.ClusterSpec.NodeTypeId
-	}
-	if int32(instance.Spec.ClusterSpec.NumWorkers) != 0 {
-		clusterSpec.NewCluster.NumWorkers = int32(instance.Spec.ClusterSpec.NumWorkers)
-	}
-	clusterSpec.Libraries = make([]dbmodels.Library, len(instance.Spec.NotebookAdditionalLibraries))
-	for i, v := range instance.Spec.NotebookAdditionalLibraries {
-		if v.Type == "jar" {
-			clusterSpec.Libraries[i].Jar = v.Properties["path"]
-		}
-		if v.Type == "egg" {
-			clusterSpec.Libraries[i].Egg = v.Properties["path"]
-		}
-		if v.Type == "whl" {
-			clusterSpec.Libraries[i].Whl = v.Properties["path"]
-		}
-		if v.Type == "pypi" {
-			clusterSpec.Libraries[i].Pypi.Package = v.Properties["package"]
-			clusterSpec.Libraries[i].Pypi.Repo = v.Properties["repo"]
-		}
-		if v.Type == "maven" {
-			clusterSpec.Libraries[i].Maven.Coordinates = v.Properties["coordinates"]
-			clusterSpec.Libraries[i].Maven.Repo = v.Properties["repo"]
-			// TODO the spec doesn't support array
-			// clusterSpec.Libraries[i].Maven.Exclusions = v.Properties["exclusions"]
-		}
-		if v.Type == "cran" {
-			clusterSpec.Libraries[i].Cran.Package = v.Properties["package"]
-			clusterSpec.Libraries[i].Cran.Repo = v.Properties["repo"]
-		}
-	}
-
-	// jobTask
-	jobTask := dbmodels.JobTask{
-		NotebookTask: &dbmodels.NotebookTask{
-			NotebookPath:   instance.Spec.NotebookTask.NotebookPath,
-			BaseParameters: make([]dbmodels.ParamPair, len(instance.Spec.NotebookSpec)),
-		},
-	}
-	counter := 0
-	for k, v := range instance.Spec.NotebookSpec {
-		jobTask.NotebookTask.BaseParameters[counter] = dbmodels.ParamPair{
-			Key: k, Value: v,
-		}
-		counter++
-	}
-
-	// timeoutSeconds
-	var timeoutSeconds = instance.Spec.TimeoutSeconds
-
-	// scopeSecrents
-	var scopeSecrets = make(map[string]string, len(instance.Spec.NotebookSpecSecrets))
-	for _, notebookSpecSecret := range instance.Spec.NotebookSpecSecrets {
-		secretName := notebookSpecSecret.SecretName
-		secret := &v1.Secret{}
-		err := r.Get(context.TODO(), types.NamespacedName{Name: secretName, Namespace: instance.Namespace}, secret)
-		if err != nil {
-			return err
-		}
-		for _, mapping := range notebookSpecSecret.Mapping {
-			secretvalue := secret.Data[mapping.SecretKey]
-			tempkey := mapping.OutputKey
-			scopeSecrets[tempkey] = fmt.Sprintf("%s", secretvalue)
-		}
-	}
-
-	secretScopeName := runName + "_scope"
-	err := r.apiClient.Secrets().CreateSecretScope(secretScopeName, "users")
-	if err != nil && !strings.Contains(err.Error(), "RESOURCE_ALREADY_EXISTS") {
-		return err
-	}
-	for k, v := range scopeSecrets {
-		err = r.apiClient.Secrets().PutSecretString(v, secretScopeName, k)
-		if err != nil {
-			return err
-		}
-	}
-
-	// submit run
-	runResponse, err := r.apiClient.Jobs().RunsSubmit(runName, clusterSpec, jobTask, int32(timeoutSeconds))
-	if err != nil {
-		return err
-	}
-
-	if runResponse.RunID == 0 {
-		return fmt.Errorf("result from API didn't return any values")
-	}
-
-	instance.Spec.NotebookTask.RunID = int(runResponse.RunID)
-	err = r.Update(context.TODO(), instance)
-	if err != nil {
-		return fmt.Errorf("error when updating NotebookJob after submitting to API: %v", err)
-	}
-
-	r.recorder.Event(instance, "Normal", "Updated", "runID added")
-	return nil
+	return r.deleteRunFromDatabricks(int64(instance.Spec.NotebookTask.RunID))
 }
