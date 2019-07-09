393da340d7e541479fb9507e2e210c1f6148a19c
Joshua Agudo <joshua.agudo@gmail.com>
2019-07-09 15:01:27 +1000

    Initial secret scopes kubebuilder create

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -76,6 +76,9 @@ var _ = BeforeSuite(func(done Done) {
 	err = databricksv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = databricksv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	// +kubebuilder:scaffold:scheme
 
 	k8sManager, err = ctrl.NewManager(cfg, ctrl.Options{
--- main.go
+++ main.go
@@ -86,6 +86,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "NotebookJob")
 		os.Exit(1)
 	}
+	err = (&controllers.SecretScopeReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SecretScope"),
+	}).SetupWithManager(mgr)
+	if err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SecretScope")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
