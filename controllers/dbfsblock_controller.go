c28d5671ebcace3f73392769ce2a557a9f810c0b
Stuart Leeks <stuartle@microsoft.com>
2019-11-26 23:17:20 +0000

    Add events for errors (#117)
    * Add events on error conditions in controllers
    
    * Run make

--- controllers/dbfsblock_controller.go
+++ controllers/dbfsblock_controller.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/go-logr/logr"
 	dbazure "github.com/xinsnake/databricks-sdk-golang/azure"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -62,27 +63,30 @@ func (r *DbfsBlockReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	if instance.IsBeingDeleted() {
 		r.Log.Info(fmt.Sprintf("HandleFinalizer for %v", req.NamespacedName))
 		if err := r.handleFinalizer(instance); err != nil {
+			r.Recorder.Event(instance, corev1.EventTypeWarning, "deleting finalizer", fmt.Sprintf("Failed to delete finalizer: %s", err))
 			return ctrl.Result{}, fmt.Errorf("error when handling finalizer: %v", err)
 		}
-		r.Recorder.Event(instance, "Normal", "Deleted", "Object finalizer is deleted")
+		r.Recorder.Event(instance, corev1.EventTypeNormal, "Deleted", "Object finalizer is deleted")
 		return ctrl.Result{}, nil
 	}
 
 	if !instance.HasFinalizer(databricksv1alpha1.DbfsBlockFinalizerName) {
 		r.Log.Info(fmt.Sprintf("AddFinalizer for %v", req.NamespacedName))
 		if err := r.addFinalizer(instance); err != nil {
+			r.Recorder.Event(instance, corev1.EventTypeWarning, "Adding finalizer", fmt.Sprintf("Failed to add finalizer: %s", err))
 			return ctrl.Result{}, fmt.Errorf("error when adding finalizer: %v", err)
 		}
-		r.Recorder.Event(instance, "Normal", "Added", "Object finalizer is added")
+		r.Recorder.Event(instance, corev1.EventTypeNormal, "Added", "Object finalizer is added")
 		return ctrl.Result{}, nil
 	}
 
 	if !instance.IsSubmitted() || !instance.IsUpToDate() {
 		r.Log.Info(fmt.Sprintf("Submit for %v", req.NamespacedName))
 		if err := r.submit(instance); err != nil {
+			r.Recorder.Event(instance, corev1.EventTypeWarning, "Submitting object", fmt.Sprintf("Failed to submit object: %s", err))
 			return ctrl.Result{}, fmt.Errorf("error when submitting DBFS block: %v", err)
 		}
-		r.Recorder.Event(instance, "Normal", "Submitted", "Object is submitted")
+		r.Recorder.Event(instance, corev1.EventTypeNormal, "Submitted", "Object is submitted")
 		return ctrl.Result{}, nil
 	}
 
